15f36dcd6540330bca90410aa4877bc5cf77b98a
mhantl <miloslav.hantl@gmail.com>
2021-04-28 10:26:21 +0200

    added Docker files

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload - with mhantl !!!
+          Edit <code>src/App.js</code> and save to reload with mhantl !!! sss mmm aaa a
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React mmmm
+          Learn React
         </a>
       </header>
     </div>
